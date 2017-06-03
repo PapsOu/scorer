9b61707c7e1f046c834ea683a8062971697b195e
PapsOu <vivien.frasca@libre-informatique.fr>
2017-06-04 00:30:49 +0200

    Manage players list (add, remove all), order, starting score, icon color

--- js/modules/players.js
+++ js/modules/players.js
@@ -1,5 +1,14 @@
 app.register({
     players: {
+
+        currentPlayer: null,
+
+        registerTemplates: function() {
+            app.core.ui.addTemplate('content', 'home', '../views/home.html');
+            app.core.ui.addTemplate('content', 'formPlayer', '../views/forms/player.html');
+            app.core.ui.addTemplate('app', 'player-actions', '../views/blocks/player-actions.html');
+        },
+
         initEvents: function() {
             var players = app.players.__get();
             if (players === null)
@@ -12,6 +21,53 @@ app.register({
             Handlebars.registerHelper('substrUcfirst', function(string, start, length) {
                 return app.core.utils.ucfirst(string.substring(start, length));
             });
+
+            Handlebars.registerHelper('json', function(object) {
+                return JSON.stringify(object);
+            });
+
+            $(document)
+
+                .on('click', '.players-list .player', function() {
+                    var item = $(this);
+                    var player = JSON.parse(item.attr('data-player'));
+
+                    if($(this).hasClass('selected')) {
+                        $('.players-list .player').removeClass('selected');
+                        app.players.currentPlayer = null;
+                    } else {
+                        $('.players-list .player').removeClass('selected');
+                        item.addClass('selected');
+                        app.players.currentPlayer = player;
+                    }
+                })
+
+                .on('click', '.players-list .player .player-icon', function(e) {
+                    e.stopImmediatePropagation();
+                    e.stopPropagation();
+                    e.preventDefault();
+                    var item = $(this).parent();
+
+                    var player = JSON.parse(item.attr('data-player'));
+                    app.baseUi.openModal('player-actions', {
+                        player: player
+                    });
+                })
+
+                .on('click', '.colorBoxContainer .colorBox', function() {
+                    var container = $(this).closest('.colorBoxContainer');
+                    container.find('.colorBox').removeClass('selected');
+
+                    var cls = $(this).attr('class').replace('colorBox ', '');
+
+                    $(this).addClass('selected');
+
+                    $('#color').val(cls);
+                })
+        },
+
+        initPlugins: function() {
+
         },
 
         __save: function(players) {
@@ -20,23 +76,51 @@ app.register({
 
         __get: function() {
             var stored = localStorage.getItem('players');
-            return (stored !== null ? JSON.parse(stored) : []);
+            var players = (stored !== null ? JSON.parse(stored) : []);
+            players.sort(function(a, b) {
+                if (a.order < b.order) {
+                    return -1;
+                } else if (a.order > b.order) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            })
+            return players;
         },
+
         ui: {
             initSortables: function() {
-                $('.players-list').sortable({
-                    handle: '.player-order',
-                    axis: "y",
-                    scroll: true,
-                    items: '.player',
-                    placeholder: 'player-moving',
-                    forcePlaceholderSize: true
-                });
+
+                if ($('.players-list .player:not(.addNewPlayer)').length > 1) {
+                    $('.players-list').sortable({
+                        handle: '.player-order',
+                        axis: "y",
+                        scroll: true,
+                        items: '.player:not(.addNewPlayer)',
+                        placeholder: 'player-moving',
+                        forcePlaceholderSize: true,
+                        stop: function(event, ui) {
+                            var players = app.players.__get();
+                            $.each($('.players-list .player:not(.addNewPlayer)'), function(i, elem) {
+                                var player = JSON.parse($(elem).attr('data-player'));
+                                $.each(players, function(j, p) {
+                                    if (player.id === p.id)
+                                        players[j].order = i + 1;
+                                });
+                            });
+                            app.players.__save(players);
+                        }
+                    });
+                }
             }
         }
     },
+
     ctrl: {
+
         states: {
+
             formPlayer: {
                 path: "/newPlayer",
                 title: "Nouveau joueur"
@@ -45,29 +129,113 @@ app.register({
 
         homeAction: function() {
             var players = app.players.__get();
-            console.info(players);
             app.core.ctrl.go('home', {
                 players: players
             }).then(function() {
                 app.players.ui.initSortables();
                 app.core.history.add(app.ctrl.states.home);
-                app.featureDiscovery.showFeatureDiscovery();
-            }, function() {
-                alert('pas ok');
+                if (app.players.currentPlayer !== null)
+                    $('.players-list .player[data-player-id="' + app.players.currentPlayer.id + '"]').trigger('click');
             });
         },
 
-        showFormPlayer: function() {
-            app.core.ctrl.render('formPlayer', {}, true).then(function() {
+        showFormPlayer: function(player) {
+            if (!isDefined(player))
+                player = null;
+            else
+                player = {
+                    player: player
+                };
 
+            app.core.ctrl.render('formPlayer', player, true).then(function() {
+                $('#name').focus();
+                $('.colorBoxContainer').randomize('.colorBox');
+                if (player === null) {
+                    setTimeout(function() {
+                        $('.colorBoxContainer .colorBox').first().trigger('click');
+                    }, 200);
+                } else {
+                    app.baseUi.closeModal();
+                    setTimeout(function() {
+                        $('.colorBoxContainer .colorBox.' + player.player.color).trigger('click');
+                    }, 200);
+                }
             });
+        },
+
+        addOrEditPlayer: function(data) {
+            console.info(data);
+            if (data.id !== null && data.id !== "") {
+                app.ctrl.editPlayer(data);
+            } else {
+                app.ctrl.addPlayer(data);
+            }
         },
 
         addPlayer: function(data) {
             var players = app.players.__get();
+
+            if (data.score === "")
+                data.score = 0;
+
+            data.name = app.core.utils.ucfirst(data.name);
+
+            data.order = players.length + 1;
+
+            data.id = moment().format('x');
+
             players.push(data);
             app.players.__save(players);
             app.ctrl.homeAction();
         },
+
+        editPlayer: function(data) {
+            var players = app.players.__get();
+
+            $.each(players, function(i, player) {
+                if (player.id === data.id) {
+                    data.name = app.core.utils.ucfirst(data.name);
+                    $.extend(true, players[i], data);
+                }
+            });
+
+            app.players.__save(players);
+            app.ctrl.homeAction();
+        },
+
+        deletePlayer: function(id) {
+            var players = app.players.__get();
+
+            $.each(players, function(i, player) {
+                if (player.id === data.id) {
+
+                    delete players[i];
+                }
+            });
+
+            app.players.__save(players);
+            app.ctrl.homeAction();
+        },
+
+        updateSettings: function(data) {
+            if (data.clearAllplayers === true) {
+                app.players.__save([]);
+            }
+            app.core.ui.toast("Paramètres enregistrés", "success");
+            app.ctrl.homeAction();
+        }
     },
 });
+
+$.fn.randomize = function(selector) {
+    var elems = selector ? $(this).find(selector) : $(this).children(),
+        parents = elems.parent();
+
+    parents.each(function() {
+        $(this).children(selector).sort(function() {
+            return Math.round(Math.random()) - 0.5;
+        }).detach().appendTo(this);
+    });
+
+    return this;
+};
